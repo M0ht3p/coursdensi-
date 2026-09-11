0c82c8038aa7ae0707e44e372f6f59e032309418
Mael <ge.akh14@gmail.com>
2026-09-11 20:07:26 +0200

    Refactor input prompts and age check logic

--- 1erscript.py
+++ 1erscript.py
@@ -1,29 +1,45 @@
 import time
+
+def calculer_temps_jeu(debut):
+    fin = time.time()
+    temps_total = fin - debut
+    minutes = int(temps_total // 60)
+    secondes = int(temps_total % 60)
+    return f"{minutes}m{secondes:02d}s"
+
+temps_debut = time.time()
+
 a = "bonjour,"
-b = input("Quelle est votre prénom? ")
-c=a+' '+b
+b = input("Quel est votre prénom? ")
+c = a + ' ' + b
 print(f"{a} votre prénom est {b}")
-d = int(input("Quel est votre age? "))
-if d > 18 :
+
+d = int(input("Quel est votre âge? "))
+if d > 18:
     print("Vous êtes majeur")
-elif d < 18 :
+elif d < 18:
     print("Vous êtes mineur")
-else :
+else:
     print("Vous avez 18 ans")
+
 time.sleep(1)
 print(f"Vous vous appelez donc {b}, et vous avez {d} ans")
 time.sleep(0.5)
-e = input("Avez vous le code? o/n ")
-if e == "o" :
-    f = input("Avez vous commencé la conduite? o/n ")
-    if f == "o" :
-        print("Ok, je ne sors plus de chez moi ")
-    elif f == "n" :
+
+e = input("Avez-vous le code? o/n ")
+if e == "o":
+    f = input("Avez-vous commencé la conduite? o/n ")
+    if f == "o":
+        print("Ok, je ne sors plus de chez moi")
+    elif f == "n":
         print("Tant mieux, je vais pouvoir sortir")
-    else :
+    else:
         print("Lettre invalide, veuillez entrer o ou n")
-else :
+else:
     time.sleep(0.6)
-    print(f"Il faut vous y mettre, {b}. ")
+    print(f"Il faut vous y mettre, {b}.")
+
 time.sleep(3)
-print("script réalisé en 7m36s")
+
+duree = calculer_temps_jeu(temps_debut)
+print(f"script réalisé en {duree}")
