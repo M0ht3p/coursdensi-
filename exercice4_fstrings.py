058953048758717cc3850d6ccce596247efed0ac
Mael <ge.akh14@gmail.com>
2026-09-22 14:43:29 +0200

    Add f-string usage for student role display

---
+++ exercice4_fstrings.py
@@ -0,0 +1,6 @@
+couleur = "blue"
+print(f"body {{ color: {couleur}; }}")
+
+eleve = {"nom": "Dupont", "role": "Délégué"}
+print(f"Le statut de {eleve['nom']} est : {eleve['role']}")
+# Erreur : Code mal parsé
